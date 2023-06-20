8275d883f26cba00897c842d8093be42f5df4a67
Khitiara <robotbrainify@gmail.com>
2023-06-20 09:29:51 -0400

    eh

--- src/Eris.Rest/AttachmentUriParser.cs
+++ src/Eris.Rest/AttachmentUriParser.cs
@@ -9,7 +9,7 @@ internal sealed partial class AttachmentUriParser : UriParser
     internal static void Register() {
         UriParser.Register(new AttachmentUriParser(), AttachmentScheme, -1);
     }
-
+    
     protected override string GetComponents(Uri uri, UriComponents uriComponents, UriFormat format) {
         if (!uri.IsAbsoluteUri)
             throw new InvalidOperationException();
@@ -19,7 +19,12 @@ internal sealed partial class AttachmentUriParser : UriParser
 
         if ((uriComponents & (UriComponents.Scheme | UriComponents.Path)) ==
             (UriComponents.Scheme | UriComponents.Path))
+            // include the second slash so Uri.ToString will do so and thus comply with discord's API docs
+            // technically this means we arent compliant with RFC 3986 but thats discord's problem at this point
             return $"attachment://{AttachmentPathRegex().Match(uri.OriginalString).Groups["path"].Value}";
+        
+        // if keeping the delimiter is kept then append :// to the scheme and/or prepend / to the path which our regex
+        // does not include in the capture group
         bool keepDelim = (uriComponents & UriComponents.KeepDelimiter) != 0;
         if ((uriComponents & UriComponents.Scheme) != 0)
             return keepDelim ? AttachmentScheme + "://" : AttachmentScheme;
@@ -33,10 +38,13 @@ internal sealed partial class AttachmentUriParser : UriParser
 
     protected override void InitializeAndValidate(Uri uri, out UriFormatException? parsingError) {
         base.InitializeAndValidate(uri, out parsingError);
+        // check for validity against our regex. very simple validation only for now but whatever
         if (!AttachmentPathRegex().IsMatch(uri.OriginalString))
             parsingError = new UriFormatException("Uri does not match discord attachment:// scheme");
     }
 
+    // we permit a single-slash version for compliance with the URI specification as specified in RFC 3986, which is
+    // adhered to by UriBuilder but not by Discord for gods know what reasons.
     [GeneratedRegex("attachment://?(?<path>.*)", RegexOptions.IgnoreCase, "en-US")]
     private static partial Regex AttachmentPathRegex();
 }
--- src/Eris.Rest/DiscordRestClient.cs
+++ src/Eris.Rest/DiscordRestClient.cs
@@ -4,18 +4,16 @@ namespace Eris.Rest;
 
 public sealed class DiscordRestClient : IDisposable
 {
-    private readonly HttpClient _client;
-
     public DiscordRestClient(HttpClient client) {
-        _client = client;
-        _client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("Eris", ThisAssembly.Info.Version));
+        HttpClient = client;
+        HttpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("Eris", ThisAssembly.Info.Version));
     }
 
-    public DiscordRestClient(IHttpClientFactory clientFactory) {
-        _client = clientFactory.CreateClient("Eris");
-    }
+    public DiscordRestClient(IHttpClientFactory clientFactory) : this(clientFactory.CreateClient("Eris.Rest")) { }
+
+    public HttpClient HttpClient { get; }
 
     public void Dispose() {
-        _client.Dispose();
+        HttpClient.Dispose();
     }
 }
